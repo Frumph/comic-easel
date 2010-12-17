8c707140133e9437baa2b7f6d5d9f2e2b3bf51ef
zerzix <adam@kelloggskorner.com>
2010-12-17 16:16:20 -0500

    admin-meta.php - 1. finished comicthumb updater. Comic thumbs are now refreashed on upload of new comic. 2. moved "comicthumb" updater JS unctions into uploader.js 3. removed all selectbox code from admin-meta.php ------------------------------------------------------- To-DO 1. make ajax driven select from for attaching comic files already in the comic directories. 2. Allow attaching of multiple selected comic files. 3. have ajax form update comicthumbs on attach

--- ceo-core.php
+++ ceo-core.php
@@ -27,7 +27,7 @@ function ceo_add_menu_pages() {
 
 	// post_type is only found on the post-new.php with $_GET, so when the $pagenow is post.php it will not be able to strictly determine the post type so it will be executed on all already made post/page edits
 	if (($_GET['post_type'] == 'comic') || ($post_type == 'comic'))   {
-		add_action('admin_head', 'comic_admin_css');
+		
 	}
 	
 	// Notice how its checking the _GET['page'], do this for the other areas
@@ -114,18 +114,23 @@ function ceo_comic_upload() {
 add_action( 'wp_ajax_ceo_thumb_update', 'ceo_comic_thumb_update' );
 
 function ceo_comic_thumb_update() {
-	echo ceo_display_comic_thumbnail('small', $_POST[post_id], false, 198); 
+	$post = get_post( $_GET['post_id']);
+	echo ceo_display_comic_thumbnail('small', $post , false, 198); 
 	exit;
 }
 
 add_action( 'wp_ajax_ceo_comic_add', 'ceo_comic_add' );
 
 function ceo_comic_add() {
-	add_post_meta($_GET[post_id], 'comic', $_GET[comicfile], false);
+$name=htmlspecialchars($_GET['name']);
+$name=stripslashes($name);
+$age=(int)$_GET['age'];
+echo "<span style='color:red'>Welcome <b>$name</b> to JavaScript Kit. So you're <b>$age</b> years old eh?</span>";
+	add_post_meta($_GET['post_id'], 'comic', $_GET['comicfile'], false);
 	exit;
 }
 
-
+add_action('admin_head', 'comic_admin_css');
 function comic_admin_css() {
 
     global $post_type;
--- js/fileuploader.js
+++ js/fileuploader.js
@@ -956,3 +956,27 @@ qq.getByClass = function(element, className){
     }
     return result;
 };
+
+var ajaxdestination="";
+
+function getdata(what,where) { // get data from source (what)
+ try {
+   xmlhttp = window.XMLHttpRequest?new XMLHttpRequest():
+  		new ActiveXObject("Microsoft.XMLHTTP");
+ }
+ catch (e) { /* do nothing */ }
+
+ document.getElementById(where).innerHTML ="<center><img src='loading.gif'></center>";
+// we are defining the destination DIV id, must be stored in global variable (ajaxdestination)
+ ajaxdestination=where;
+ xmlhttp.onreadystatechange = triggered; // when request finished, call the function to put result to destination DIV
+ xmlhttp.open("GET", what);
+ xmlhttp.send(null);
+  return false;
+}
+
+function triggered() { // put data returned by requested URL to selected DIV
+  if (xmlhttp.readyState == 4) if (xmlhttp.status == 200) 
+    document.getElementById(ajaxdestination).innerHTML =xmlhttp.responseText;
+}
+
