035c4360c347d86a995dc7367d328c02298febdd
Frumph <philip@frumph.net>
2013-04-23 07:02:29 -0700

    check to see if there IS a secondary image, else default back to featured/primary

--- widgets/thumbnail.php
+++ widgets/thumbnail.php
@@ -41,7 +41,16 @@ class ceo_thumbnail_widget extends WP_Widget {
 					echo '<div class="comic-thumb comic-thumb-'.$post->ID.'">';
 					if ($instance['centering']) echo "\r\n<center>\r\n";
 					if ($instance['secondary'] && class_exists('MultiPostThumbnails')) {
-						echo "<a href=\"".$the_permalink."\" rel=\"bookmark\" title=\"Permanent Link to ".get_the_title()."\">".MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL,  'secondary-image')."</a>\r\n";
+						$secondary_image = MultiPostThumbnails::get_the_post_thumbnail(get_post_type(), 'secondary-image', NULL,  'secondary-image');
+						if (!empty($url)) {
+							echo "<a href=\"".$the_permalink."\" rel=\"bookmark\" title=\"Permanent Link to ".get_the_title()."\">".$secondary_image."</a>\r\n";
+						} else {
+							if ( has_post_thumbnail($post->ID) ) {
+								echo "<a href=\"".$the_permalink."\" rel=\"bookmark\" title=\"Permanent Link to ".get_the_title()."\">".get_the_post_thumbnail($post->ID, 'thumbnail')."</a>\r\n";
+							} else {
+								echo "No Thumbnail Found.";	
+							}							
+						}
 					} else {
 						if ( has_post_thumbnail($post->ID) ) {
 							echo "<a href=\"".$the_permalink."\" rel=\"bookmark\" title=\"Permanent Link to ".get_the_title()."\">".get_the_post_thumbnail($post->ID, 'thumbnail')."</a>\r\n";
