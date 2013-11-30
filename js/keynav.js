188f8e6e0c4242c2168ba82fbe803807939402a8
Frumph <philip@frumph.net>
2013-11-29 20:37:03 -0800

    Apostrophy fix for keynav.js file

--- js/keynav.js
+++ js/keynav.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function () {
 	jQuery(document).keydown(function(e) {
 		var url = false;
 
-		if(document.querySelector(‘#comment:focus,#author:focus,#email:focus,#url:focus,#mcspvalue:focus’)) return;
+		if(document.querySelector('#comment:focus,#author:focus,#email:focus,#url:focus,#mcspvalue:focus')) return;
 
 		if (e.which == 37) {  // Left arrow key code
 			url = jQuery('a.comic-nav-previous').attr('href');
