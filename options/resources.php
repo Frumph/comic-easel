67a6446feda3403db94ad8fcec8568b41a1cbb64
Frumph <philip@frumph.net>
2014-10-23 04:27:22 -0700

    removed resources.php

--- options/resources.php
+++
@@ -1,9 +0,0 @@
-<div id="comiceasel-resources">
-		<div class="comiceasel-options">
-				<?php 
-					$got_file = wp_remote_get( 'http://frumph.net/downloads/index.html' );
-					if (isset($got_file['body']) && !empty($got_file['body'])) echo $got_file['body'];
-				?>
-			<br />
-		</div>
-</div>
