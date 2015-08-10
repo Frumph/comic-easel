d7f89b5ff55cb1f83d9ff57144d667a8f647a574
Frumph <philip@frumph.net>
2015-08-10 05:31:58 -0700

    Check if window has focus first.

--- js/keynav.js
+++ js/keynav.js
@@ -1,17 +1,20 @@
 jQuery(document).ready(function () {
- 
-	jQuery(document).keydown(function(e) {
-		var url = false;
+	var window_focus;
 
-		if(document.querySelector('#comment:focus,#author:focus,#email:focus,#url:focus,#mcspvalue:focus')) return;
+	jQuery(window).focus(function() {
+		jQuery(document).keydown(function(e) {
+			var url = false;
 
-		if (e.which == 37) {  // Left arrow key code
-			url = jQuery('a.comic-nav-previous').attr('href');
-		} else if (e.which == 39) {  // Right arrow key code
-			url = jQuery('a.comic-nav-next').attr('href');
-		}
-		if (url) {
-			window.location = url;
-		}
+			if(document.querySelector('#comment:focus,#author:focus,#email:focus,#url:focus,#mcspvalue:focus')) return;
+
+			if (e.which == 37) {  // Left arrow key code
+				url = jQuery('a.comic-nav-previous').attr('href');
+			} else if (e.which == 39) {  // Right arrow key code
+				url = jQuery('a.comic-nav-next').attr('href');
+			}
+			if (url) {
+				window.location = url;
+			}
+		});
 	});
 });
